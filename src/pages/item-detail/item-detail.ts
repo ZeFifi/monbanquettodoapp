b90b76c1d29911a700d81667fc4478646792a5d9
Philippe Pinceloup <philippe@soshape.com>
2017-10-11 17:43:50 +0200

    Chronometer works now

--- src/pages/item-detail/item-detail.ts
+++ src/pages/item-detail/item-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
+import Timer from 'easytimer.js';
 
 @Component({
   selector: 'page-item-detail',
@@ -11,12 +12,36 @@ export class ItemDetailPage {
   title;
   description;
 
-  constructor(public navParams: NavParams){
+  constructor(public navParams: NavParams) {
 
   }
 
   ionViewDidLoad() {
     this.title = this.navParams.get('item').title;
     this.description = this.navParams.get('item').description;
+
+    var timer = new Timer();
+    $('#chronoExample .startButton').click(function() {
+      timer.start();
+    });
+    $('#chronoExample .pauseButton').click(function() {
+      timer.pause();
+    });
+    $('#chronoExample .stopButton').click(function() {
+      timer.stop();
+    });
+    $('#chronoExample .resetButton').click(function() {
+      timer.reset();
+    });
+    timer.addEventListener('secondsUpdated', function(e) {
+      $('#chronoExample .values').html(timer.getTimeValues().toString());
+    });
+    timer.addEventListener('started', function(e) {
+      $('#chronoExample .values').html(timer.getTimeValues().toString());
+    });
+    timer.addEventListener('reset', function(e) {
+      $('#chronoExample .values').html(timer.getTimeValues().toString());
+    });
   }
+
 }
